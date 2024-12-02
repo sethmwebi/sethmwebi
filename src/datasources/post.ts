ac8919e843ab7464a8585fc93a1b64679b7c3449
Seth Mwebi <sethmwebi27@gmail.com>
2024-12-02 21:03:37 +0300

    add post datasource

---
+++ src/datasources/post.ts
@@ -0,0 +1,84 @@
+import { PrismaClient, Post } from "@prisma/client";
+
+export class PostAPI {
+  prisma: PrismaClient;
+  context: any;
+
+  constructor({ prisma }: { prisma: PrismaClient }) {
+    this.prisma = prisma;
+  }
+
+  initialize(config: { context: any }) {
+    this.context = config.context;
+  }
+
+  async createPost(data: {
+    title: string;
+    content: string;
+    imageUrl?: string;
+    authorId: string;
+  }): Promise<Post> {
+    try {
+      return await this.prisma.post.create({
+        data: {
+          title: data.title,
+          content: data.content,
+          imageUrl: data.imageUrl || null,
+          author: { connect: { id: data.authorId } },
+        },
+      });
+    } catch (error) {
+      console.error("Error creating post: ", error);
+      throw error;
+    }
+  }
+
+  async getPostById(postId: string): Promise<Post | null> {
+    try {
+      return await this.prisma.post.findUnique({
+        where: { id: postId },
+        include: { author: true, comments: true, likes: true, tags: true },
+      });
+    } catch (error) {
+      console.error("Error fetching post by ID:", error);
+      return null;
+    }
+  }
+
+  async updatePost(
+    postId: string,
+    data: Partial<Omit<Post, "id" | "createdAt" | "updatedAt">>,
+  ): Promise<Post | null> {
+    try {
+      return await this.prisma.post.update({
+        where: { id: postId },
+        data,
+      });
+    } catch (error) {
+      console.error("Error updating post:", error);
+      return null;
+    }
+  }
+
+  async deletePost(postId: string): Promise<Post | null> {
+    try {
+      return await this.prisma.post.delete({
+        where: { id: postId },
+      });
+    } catch (error) {
+      console.error("Error deleting post: ", error);
+      return null;
+    }
+  }
+
+  async getAllPosts(): Promise<Post[]> {
+    try {
+      return await this.prisma.post.findMany({
+        include: { author: true, comments: true, tags: true, likes: true },
+      });
+    } catch (error) {
+      console.error("Error fetching posts: ", error);
+      return [];
+    }
+  }
+}
